97da95d2bbbf3fa83533aa6b0317c5bc22b95b05
Tiven <vairtor@gmail.com>
2023-06-13 00:14:39 -0300

    🚚 Move routes to locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,29 +5,38 @@ import RollerCoaster from "@/components/icons/RollerCoaster";
 import ThemeButton from "@/components/navBar/ThemeButton";
 import LanguageButton from "@/components/navBar/LanguageButton";
 
+import { useLocale } from "next-intl";
+import { notFound } from "next/navigation";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "HackaCode - Theme Park",
 	description:
 		"A website developed for the HackaCode tournament.\
-  It consists in managing a park of dirversions, sales, employees, clients, etc. ",
+  		It consists in managing a park of dirversions, sales, employees, clients, etc. ",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children, params }: { children: React.ReactNode }) {
+	const locale = useLocale();
+
+	if (params.locale !== locale) {
+		notFound();
+	}
+
 	return (
-		<html lang="en">
+		<html lang={locale}>
 			<body className={inter.className}>
 				<header className="flex p-3 justify-between items-center">
 					<div>
-						<Link href={"/"} className="font-bold flex gap-2 items-center px-7 text-lg">
+						<Link href="/" className="font-bold flex gap-2 items-center px-7 text-lg">
 							CodeCrafters
 							<RollerCoaster className="w-9 h-9 fill-white mb-1" />
 						</Link>
 					</div>
 					<div className="px-7 flex gap-3">
 						<ThemeButton />
-						<LanguageButton />
+						<LanguageButton locale={locale} />
 					</div>
 				</header>
 				{children}
---
+++ src/app/[locale]/login/page.tsx
@@ -0,0 +1,39 @@
+import { useTranslations } from "next-intl";
+interface Props {}
+
+const Login: React.FC<Props> = () => {
+	const loginMessages = useTranslations("Login");
+	return (
+		<main className="flex min-h-screen flex-col justify-center">
+			<form action="" className="bg-white mx-auto flex flex-col rounded-lg p-8 gap-4">
+				<div className="flex flex-col">
+					<label htmlFor="username" className="text-slate-950">
+						{loginMessages("username")}
+					</label>
+					<input
+						type="text"
+						name="username"
+						id="username"
+						className="bg-slate-200 rounded-sm p-2 text-black"
+					/>
+				</div>
+				<div className="flex flex-col">
+					<label htmlFor="password" className="text-slate-950">
+						{loginMessages("password")}
+					</label>
+					<input
+						type="text"
+						name="password"
+						id="password"
+						className="bg-slate-200 rounded-sm p-2 text-black"
+					/>
+				</div>
+				<button className="bg-slate-500 text-white rounded-sm py-2">
+					{loginMessages("loginButton")}
+				</button>
+			</form>
+		</main>
+	);
+};
+
+export default Login;
