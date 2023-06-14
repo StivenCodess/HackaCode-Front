2d678cfb6ab3219f14895ac689f0fc93cd362e34
Tiven <vairtor@gmail.com>
2023-06-14 19:09:34 -0300

    🔇 Remove locale log

--- src/components/navBar/LanguageButton.tsx
+++ src/components/navBar/LanguageButton.tsx
@@ -4,23 +4,18 @@ import Link from "next-intl/link";
 import { usePathname } from "next-intl/client";
 
 interface Props {
-  locale: string;
+	locale: string;
 }
 
 const LanguageButton: React.FC<Props> = ({ locale }) => {
-  const pathname = usePathname();
-  console.log(locale);
-  return (
-    <div className="flex w-9 h-8 bg-white rounded-lg gap-3">
-      <Link
-        href={pathname}
-        locale={locale === "es" ? "en" : "es"}
-        className="text-black"
-      >
-        {(locale === "es" ? "en" : "es").toUpperCase()}
-      </Link>
-    </div>
-  );
+	const pathname = usePathname();
+	return (
+		<div className="flex w-9 h-8 bg-white rounded-lg gap-3">
+			<Link href={pathname} locale={locale === "es" ? "en" : "es"} className="text-black">
+				{(locale === "es" ? "en" : "es").toUpperCase()}
+			</Link>
+		</div>
+	);
 };
 
 export default LanguageButton;
