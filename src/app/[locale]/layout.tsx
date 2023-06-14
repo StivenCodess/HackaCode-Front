79d1e3d55023e119ac15039cef746df16dc52d12
Tiven <vairtor@gmail.com>
2023-06-14 19:57:33 -0300

    ♻️ Create header component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,11 @@
-import Link from "next/link";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import RollerCoaster from "@/components/icons/RollerCoaster";
-import ThemeButton from "@/components/Header/ThemeButton";
-import LanguageButton from "@/components/Header/LanguageButton";
 
 import { useLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { LayoutProps } from "@/interfaces/Layout";
+
+import Header from "@/components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,18 +26,7 @@ export default function RootLayout({ children, params }: LayoutProps) {
 	return (
 		<html lang={locale}>
 			<body className={inter.className}>
-				<header className="flex p-3 justify-between items-center">
-					<div>
-						<Link href="/" className="font-bold flex gap-2 items-center px-7 text-lg">
-							CodeCrafters
-							<RollerCoaster className="w-9 h-9 fill-white mb-1" />
-						</Link>
-					</div>
-					<div className="px-7 flex gap-3">
-						<ThemeButton />
-						<LanguageButton locale={locale} />
-					</div>
-				</header>
+				<Header locale={locale}></Header>
 				{children}
 			</body>
 		</html>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import RollerCoaster from "@/components/icons/RollerCoaster";
+import ThemeButton from "@/components/Header/ThemeButton";
+import LanguageButton from "@/components/Header/LanguageButton";
+
+interface Props {
+	locale: string;
+}
+
+const InputForm: React.FC<Props> = ({ locale }) => {
+	return (
+		<header className="flex p-3 justify-between items-center">
+			<div>
+				<Link href="/" className="font-bold flex gap-2 items-center px-7 text-lg">
+					CodeCrafters
+					<RollerCoaster className="w-9 h-9 fill-white mb-1" />
+				</Link>
+			</div>
+			<div className="px-7 flex gap-3">
+				<ThemeButton />
+				<LanguageButton locale={locale} />
+			</div>
+		</header>
+	);
+};
+
+export default InputForm;
