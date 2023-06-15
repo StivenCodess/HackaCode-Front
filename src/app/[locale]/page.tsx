6b13e5d820ce51aa9bc3d59410f76fce27b33927
Tiven <vairtor@gmail.com>
2023-06-15 00:38:42 -0300

    💄 Fix padding of home

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
 	const homeMessages = useTranslations("Home");
 	return (
 		<main className="flex justify-center items-center min-h-screen">
-			<div className="bg-white w-80 h-80 rounded-lg grid place-content-center gap-7">
+			<div className="bg-white p-12 rounded-lg grid place-content-center gap-7 ">
 				<FontAwesomeIcon icon={faWrench} size="3x" className="text-black  mx-auto" />
-				<h1 className="text-black text-2xl font-medium">{homeMessages("work")}</h1>
+				<h1 className="text-black text-2xl font-medium ">{homeMessages("work")}</h1>
 			</div>
 		</main>
 	);
