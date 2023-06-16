efc2b3cd4339a761af0ab6c9fd4351a680da7485
Tiven <vairtor@gmail.com>
2023-06-15 22:42:24 -0300

    💄 Change text color to white

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,11 @@ const InputForm: React.FC<Props> = ({ locale }) => {
 	return (
 		<header className="flex p-3 justify-between items-center absolute w-screen">
 			<div>
-				<Link href="/" className="font-bold flex gap-2 items-center px-7 text-lg">
+				<Link
+					href="/"
+					className="font-bold flex gap-2 items-center px-7 text-lg text-white
+								hover:scale-105 transition-all"
+				>
 					CodeCrafters
 					<RollerCoaster className="w-9 h-9 fill-white mb-1" />
 				</Link>
