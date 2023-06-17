8252fe86f8c5ea7d273333e17f1a42e8802143d4
Tiven <vairtor@gmail.com>
2023-06-16 21:05:55 -0300

    ✨ Add cloud2 component

---
+++ src/components/icons/Cloud2.tsx
@@ -0,0 +1,15 @@
+import { SVGProps } from "react";
+const Cloud2 = (props: SVGProps<SVGSVGElement>) => (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 278.56 204.81" {...props}>
+		<g data-name="Capa 2">
+			<path
+				d="M278.56 101.84A55.83 55.83 0 0 0 222.74 46c-1.11 0-2.22.05-3.32.11a66.92 66.92 0 0 0-118.12-18 31.3 31.3 0 0 0-49.61 25.42v.16a55.82 55.82 0 1 0 32.2 103.91 57 57 0 0 0 103.63 21.84 31.29 31.29 0 0 0 47.67-23.19 55.83 55.83 0 0 0 43.37-54.41Z"
+				data-name="Graphic Elements"
+				style={{
+					fill: `${props.fill}`,
+				}}
+			/>
+		</g>
+	</svg>
+);
+export default Cloud2;
