ff9cd1ddd717fe4f60744a305bdf4389c2d0fcc3
Tiven <vairtor@gmail.com>
2023-06-16 21:05:46 -0300

    ✨ Add cloud component

---
+++ src/components/icons/Cloud.tsx
@@ -0,0 +1,15 @@
+import { SVGProps } from "react";
+const Cloud = (props: SVGProps<SVGSVGElement>) => (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 332.05 207" {...props}>
+		<g data-name="Capa 2">
+			<path
+				d="M291.44 64.68a40.57 40.57 0 0 0-16 3.26 48.57 48.57 0 0 0-75.66-32 69.35 69.35 0 0 0-130 30.82 47.15 47.15 0 1 0-22.62 88.53h.27a57.26 57.26 0 0 0 107.17 22.02 37.54 37.54 0 0 0 41-5.87 45.26 45.26 0 0 0 80.55-28.34v-.22a40.6 40.6 0 1 0 15.33-78.2Z"
+				data-name="Graphic Elements"
+				style={{
+					fill: `${props.fill}`,
+				}}
+			/>
+		</g>
+	</svg>
+);
+export default Cloud;
