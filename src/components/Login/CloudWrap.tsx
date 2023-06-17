ba81b0c93f0606c4e063887f08056651f357daf5
Tiven <vairtor@gmail.com>
2023-06-16 22:39:20 -0300

    📱 Add decoration on Mobile

--- src/components/Login/CloudWrap.tsx
+++ src/components/Login/CloudWrap.tsx
@@ -13,12 +13,12 @@ const CloudWrap: React.FC<Props> = () => {
 				fill="#fff"
 			></Cloud>
 			<Cloud
-				className="hidden md:block absolute w-24 h-24 top-[37%] md:left-[72%] xl:left-[60%] 2xl:left-[60%]
+				className="top-[20%] left-[75%] md:block absolute w-24 h-24 md:top-[37%] md:left-[72%] xl:left-[60%] 2xl:left-[60%]
 				opacity-10 xl:w-56 z-10 transition-all hover:scale-125 duration-500 drop-shadow-sm"
 				fill="#fff"
 			></Cloud>
 			<Cloud2
-				className="hidden md:block absolute w-32 h-32 top-[65%] lg:top-[68%] md:left-[60%] xl:left-[52%] 2xl:left-[52%]
+				className="top-[68%] absolute w-32 h-32 md:top-[65%] lg:top-[68%] md:left-[60%] xl:left-[52%] 2xl:left-[52%]
 				opacity-30 xl:w-56 z-10 transition-all hover:scale-125 duration-500 drop-shadow-sm"
 				fill="#fff"
 			></Cloud2>
@@ -27,8 +27,8 @@ const CloudWrap: React.FC<Props> = () => {
 				opacity-10 xl:w-56 z-10 transition-all hover:scale-125 duration-500 drop-shadow-sm"
 				fill="#fff"
 			></Cloud2>
-			<Ballon className="hidden md:block absolute w-32 h-32 opacity-10 top-[13%] left-[5%] m lg:top-[15%] lg:left-[20%] hover:scale-125 transition-all duration-500"></Ballon>
-			<Ballon className="hidden md:block absolute w-24 h-24 opacity-30 top-[50%] left-[75%] lg:top-[45%] lg:left-[75%] hover:scale-150 transition-all duration-500"></Ballon>
+			<Ballon className="md:block absolute w-32 h-32 opacity-10 top-[13%] left-[5%] m lg:top-[15%] lg:left-[20%] hover:scale-125 transition-all duration-500"></Ballon>
+			<Ballon className="md:block absolute w-24 h-24 opacity-30 top-[50%] left-[75%] lg:top-[45%] lg:left-[75%] hover:scale-150 transition-all duration-500"></Ballon>
 		</>
 	);
 };
