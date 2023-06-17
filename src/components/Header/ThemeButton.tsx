84a60db8c192c8ca9399434aa33773aa195de30a
Tiven <vairtor@gmail.com>
2023-06-16 21:10:10 -0300

    🔥 Remove import of useState

--- src/components/Header/ThemeButton.tsx
+++ src/components/Header/ThemeButton.tsx
@@ -3,7 +3,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 interface Props {}
 
