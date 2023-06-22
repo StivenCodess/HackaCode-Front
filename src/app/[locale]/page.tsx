d0c5ee954e37e76fa7b4d37ac197368118a43553
Tiven <vairtor@gmail.com>
2023-06-22 00:11:27 -0300

    🌐 set translation to welcome modal

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,27 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWrench } from "@fortawesome/free-solid-svg-icons";
+import ProfileCard from "@/components/Home/ProfileCard/ProfileCard";
+import ModalWelcome from "@/components/Home/WelcomeModal/ModalWelcome";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
 	const homeMessages = useTranslations("Home");
+	const welcomeTranslation = useTranslations("Welcome");
+
+	const welcomeMessage = {
+		message: welcomeTranslation("message"),
+		by: welcomeTranslation("by"),
+		messageContinue: welcomeTranslation("messageContinue"),
+	};
+
 	return (
-		<main className="flex justify-center items-center h-screen">
-			<div className="bg-white p-12 rounded-lg grid place-content-center gap-7 ">
-				<FontAwesomeIcon icon={faWrench} size="3x" className="text-black  mx-auto" />
-				<h1 className="text-black text-2xl font-medium ">{homeMessages("work")}</h1>
-			</div>
+		<main className="flex flex-col justify-start items-center h-screen gap-20">
+			<ModalWelcome
+				welcomeMessage={welcomeMessage}
+				welcomeHeaderMessage={welcomeTranslation("header")}
+			/>
+			<h2 className="mt-[150px] text-4xl bg-white dark:bg-gray-700 rounded p-3 px-10 text-center text-black dark:text-white hover:-translate-y-3 transition-all duration-1000">
+				Welcome Jhoe!
+			</h2>
+			<ProfileCard />
 		</main>
 	);
 }
