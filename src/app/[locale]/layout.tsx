1c5f255610f6dd6c46c3c71063c29db8d3a1bb25
Tiven <vairtor@gmail.com>
2023-06-22 00:10:20 -0300

    🎨 format the code

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,11 @@
 import "./globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { Inter } from "next/font/google";
-import { config } from "@fortawesome/fontawesome-svg-core";
 import { useLocale } from "next-intl";
 import { LayoutProps } from "@/interfaces/Layout";
 import { notFound } from "next/navigation";
-
 import Header from "@/components/Header/Header";
 import Providers from "./providers";
-config.autoAddCss = false;
-const inter = Inter({ subsets: ["latin"] });
+import { roboto } from "@/app/utils/robotoFont";
 
 export const metadata = {
 	title: "HackaCode - Theme Park",
@@ -20,14 +16,11 @@ export const metadata = {
 
 export default function RootLayout({ children, params }: LayoutProps) {
 	const locale = useLocale();
-
-	if (params.locale !== locale) {
-		notFound();
-	}
+	if (params.locale !== locale) notFound();
 
 	return (
-		<html lang={locale}>
-			<body className=" bg-gradient-to-b from-[#f4ccff] to-[#88a1fb] dark:from-[#424ea2] dark:to-[#7540b2] overflow-hidden">
+		<html lang={locale} className={roboto.variable}>
+			<body className="font-roboto bg-gradient-to-b from-[#f4ccff] to-[#88a1fb] dark:from-[#424ea2] dark:to-[#7540b2] overflow-hidden">
 				<Providers>
 					<Header locale={locale}></Header>
 					{children}
