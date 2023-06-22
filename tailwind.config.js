08fef60edc40faead0f3283394f06b6cf6338c57
Tiven <vairtor@gmail.com>
2023-06-21 23:54:58 -0300

    🔧 Add font to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 	],
 	darkMode: "class",
 	theme: {
+		fontFamily: {
+			roboto: "var(--font-roboto)",
+		},
 		extend: {
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
