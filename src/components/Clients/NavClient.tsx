4bf7bcb8a94bc101e347ab7332d4539f445b069e
Pablo <pabloesanabriaq@gmail.com>
2023-06-28 22:48:02 -0300

    🏷️  Add: types

--- src/components/Clients/NavClient.tsx
+++ src/components/Clients/NavClient.tsx
@@ -1,34 +1,44 @@
 import Option from "./Option";
 
-import { faPenToSquare, faList, faPlus } from "@fortawesome/free-solid-svg-icons";
-const NavClient = ({ handleCLick, status }) => {
-	return (
-		<nav className="w-[30%] ">
-			<ul className="flex flex-col items-center font-light justify-center ">
-				<Option
-					handleClick={handleCLick}
-					message={"Register client"}
-					optionName={"register"}
-					status={status}
-					icon={faPlus}
-				/>
-				<Option
-					handleClick={handleCLick}
-					message={"View clients"}
-					optionName={"list"}
-					status={status}
-					icon={faList}
-				/>
-				<Option
-					handleClick={handleCLick}
-					message={"Edit client"}
-					optionName={"edit"}
-					status={status}
-					icon={faPenToSquare}
-				/>
-			</ul>
-		</nav>
-	);
+import {
+  faPenToSquare,
+  faList,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+
+interface Props {
+  handleCLick: (componentName: string) => void;
+  status: string;
+}
+
+const NavClient = ({ handleCLick, status }: Props) => {
+  return (
+    <nav className="w-[30%] ">
+      <ul className="flex flex-col items-center font-light justify-center ">
+        <Option
+          handleClick={handleCLick}
+          message={"Register client"}
+          optionName={"register"}
+          status={status}
+          icon={faPlus}
+        />
+        <Option
+          handleClick={handleCLick}
+          message={"View clients"}
+          optionName={"list"}
+          status={status}
+          icon={faList}
+        />
+        <Option
+          handleClick={handleCLick}
+          message={"Edit client"}
+          optionName={"edit"}
+          status={status}
+          icon={faPenToSquare}
+        />
+      </ul>
+    </nav>
+  );
 };
 
 export default NavClient;
--- src/components/Home/ButtonTy/ButtonThanks.tsx
+++ src/components/Home/ButtonTy/ButtonThanks.tsx
@@ -2,21 +2,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-const ButtonThanks = ({ setStatus, setLeave }) => {
-	const handleClick = () => {
-		localStorage.setItem("welcome", JSON.stringify(true));
-		setLeave(false);
-		setStatus(true);
-	};
+interface Props {
+  setStatus: (status: boolean) => void;
+  setLeave: (leave: boolean) => void;
+}
 
-	return (
-		<button
-			onClick={handleClick}
-			className="absolute bottom-0 right-0 m-5 p-1 px-2 rounded dark:text-black dark:bg-white/60 text-center font-light bg-black/30 text-white hover:scale-105 transition-all duration-300"
-		>
-			Thanks <FontAwesomeIcon icon={faHeart} size="1x" />
-		</button>
-	);
+const ButtonThanks = ({ setStatus, setLeave }: Props) => {
+  const handleClick = () => {
+    localStorage.setItem("welcome", JSON.stringify(true));
+    setLeave(false);
+    setStatus(true);
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="absolute bottom-0 right-0 m-5 p-1 px-2 rounded dark:text-black dark:bg-white/60 text-center font-light bg-black/30 text-white hover:scale-105 transition-all duration-300"
+    >
+      Thanks <FontAwesomeIcon icon={faHeart} size="1x" />
+    </button>
+  );
 };
 
 export default ButtonThanks;
